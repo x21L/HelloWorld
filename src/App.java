c2a54612a6cd1064fdd7991bb69226ecbce6d1cc
lukas <lukas.wais@outlook.de>
2020-09-19 12:07:51 +0200

    added App

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Animal parrot = new Animal("Ludwig",2, "blue", "bird");
+        parrot.print();
+
+        Animal fox = new Animal("Foxi", 4, "orange", "mammal");
+        fox.print();
+    }
+}
